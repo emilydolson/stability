98a87b94003e316b09af78d166e70484fcbf9cb7
Emily Dolson <EmilyLDolson@gmail.com>
2017-01-26 22:47:27 -0500

    Added database

--- server/server.py
+++ server/server.py
@@ -1,10 +1,20 @@
-from bottle import Bottle, route, run, static_file
+from bottle import route, run, static_file
+import bottle
 
-app = Bottle()
+app = bottle.Bottle()
+plugin = bottle.ext.sqlite.Plugin(dbfile="user_settings.db")
 
 
 @app.route("/<filepath:path>")
 def server_static(filepath):
     return static_file(filepath, root='/home/ec2-user/stability')
+
+
+@app.route('/show/:item')
+def show(item, db):
+    row = db.execute('SELECT * from items where name=?', item).fetchone()
+    if row:
+        return template('showitem', page=row)
+    return HTTPError(404, "Page not found")
 
 run(app, port=80, host='0.0.0.0', debug=True)
