774ea90c1abf7bba1e2f51482c1d850e39968f81
Emily Dolson <EmilyLDolson@gmail.com>
2017-01-29 01:41:25 -0500

    handle all sleep numbers

--- js/main.js
+++ js/main.js
@@ -155,7 +155,9 @@ function graphSleepData(){
       sleep_data.push([new Date(+all_activity_data[i].dataset[0].point[0].startTimeNanos/1000000), 0]);
     }
     for (j in all_activity_data[i].dataset[0].point){
+      console.log(all_activity_data[i].dataset[0].point[j].value[0].intVal);
       if (SLEEP_NUMBERS.includes(+all_activity_data[i].dataset[0].point[j].value[0].intVal)) {
+        console.log("adding");
         sleep_data[sleep_data.length-1][1] += +all_activity_data[i].dataset[0].point[j].value[1].intVal/3600000;
       }
     }
