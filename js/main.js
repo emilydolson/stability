f3b8eb9e4e5d986bf4ec88f65b45bbac33b787ec
Emily Dolson <EmilyLDolson@gmail.com>
2017-01-29 01:37:42 -0500

    handle all sleep numbers

--- js/main.js
+++ js/main.js
@@ -150,7 +150,9 @@ function getActivity(){
 function graphSleepData(){
   sleep_data = [];
   for (i in all_activity_data) {
-    sleep_data.push([new Date(+all_activity_data[i].dataset[0].point[j].startTimeNanos/1000000), 0]);
+    if (all_activity_data[i].dataset[0].point.length > 0) {
+      sleep_data.push([new Date(+all_activity_data[i].dataset[0].point[0].startTimeNanos/1000000), 0]);
+    }
     for (j in all_activity_data[i].dataset[0].point){
       if (SLEEP_NUMBERS.includes(all_activity_data[i].dataset[0].point[j].value[0].intVal)) {
         sleep_data[sleep_data.length-1][1]+=all_activity_data[i].dataset[0].point[j].value[1].intVal/3600000;
