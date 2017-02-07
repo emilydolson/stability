60668e1f116650558b953737dda51632878f9b27
Emily Dolson <EmilyLDolson@gmail.com>
2017-02-07 16:27:34 -0500

    Fixing back height

--- js/main.js
+++ js/main.js
@@ -475,5 +475,5 @@ function makeGraph(data, svg_id, options){
       .attr("class", "left axis")
       .call(yAxis);
 
-  graphs[id_without_hash] = {"x_axis":xAxis, "y_axis":yAxis, "vis":vis, "options":options};
+  graphs[id_without_hash] = {"x_axis":xAxis, "y_axis":yAxis, "vis":svg, "options":options};
 }
