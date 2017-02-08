b5bd8a96b2581d831a245e66f8ca6f2e0e2d4bc7
Emily Dolson <EmilyLDolson@gmail.com>
2017-02-07 21:01:57 -0500

    simplifying callback structure

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ var NON_ACTIVE_NUMBERS = [0, 3, 4, 5, 72, 109, 110, 111, 112] //2 = on foot... n
 var SLEEP_NUMBERS = [72, 109, 110, 111]
 var DAY = 86400000 //number of milliseconds in a day
 
-var weight_data;
-var all_activity_data;
+var all_data = {};
 var id_token;
 var graphs = {};
 
@@ -32,6 +31,10 @@ function onSignIn(googleUser) {
       .text("Sign out");
 
   loadGapi();
+}
+
+function loadGapi() {
+  gapi.load('client', getPreferences);
 }
 
 function onFailure(error) {
@@ -220,11 +223,19 @@ function makeTimePeriodRadioButton(list_el, variable, unit, checked) {
 
 function callMakeGraph(variable, options){
   if (variable == "Sleep") {
-    graphSleepData(options);
+    getActivity( function() {
+      calcSleepTimeData(all_data);
+      makeGraph(all_data.sleeptime, "#Sleep-graph", options);
+    });
   } else if (variable == "Weight") {
-    makeGraph(weight_data, "#Weight-graph", options);
+    getWeight(function(){
+      makeGraph(weight_data, "#Weight-graph", options);
+    })
   } else if (variable == "Active-time") {
-    graphTotalActivityData(options);
+    getActivity( function() {
+      calcActiveTimeData(all_data);
+      makeGraph(all_data.activetime, "#Active-time-graph", options);
+    });
   }
   updatePreferences();
 }
@@ -259,9 +270,22 @@ function updateSmoothness(variable) {
   callMakeGraph(variable, g.options);
 }
 
+// function loadData() {
+//   // d3.selectAll(".getdata").classed("disabled", false);
+//   getWeight();
+//   getActivity();
+// }
+
+
+
+function getDataFromStream(streamId) {
+  console.log("https://www.googleapis.com/fitness/v1/users/me/dataSources/"+streamId+"/datasets/0-"+CURR_TIME);
+  return gapi.client.request("https://www.googleapis.com/fitness/v1/users/me/dataSources/"+streamId+"/datasets/0-"+CURR_TIME);
+}
+
 function getWeight(callback) {
 
-  if (weight_data) {
+  if (all_data.weight_data) {
     callback();
     return;
   }
@@ -285,7 +309,7 @@ function getWeight(callback) {
           // Extract data from JSON object and convert kg to pounds
           data[el] = [new Date(+data[el].endTimeNanos/1000000), data[el].value[0].fpVal*2.20462];
         }
-        weight_data = data;
+        all_data.weight_data = data;
         callback();
       });
   }, function(reason) {
@@ -293,23 +317,8 @@ function getWeight(callback) {
   });
 };
 
-// function loadData() {
-//   // d3.selectAll(".getdata").classed("disabled", false);
-//   getWeight();
-//   getActivity();
-// }
-
-function loadGapi() {
-  gapi.load('client', getPreferences);
-};
-
-function getDataFromStream(streamId) {
-  console.log("https://www.googleapis.com/fitness/v1/users/me/dataSources/"+streamId+"/datasets/0-"+CURR_TIME);
-  return gapi.client.request("https://www.googleapis.com/fitness/v1/users/me/dataSources/"+streamId+"/datasets/0-"+CURR_TIME);
-}
-
 function getActivity(callback){
-  if (all_activity_data) {
+  if (all_data.all_activity_data) {
     callback();
     return;
   }
@@ -335,55 +344,15 @@ function getActivity(callback){
     }
   });
   activity_data.then(function(response){
-    all_activity_data = response.result.bucket;
+    all_data.all_activity_data = response.result.bucket;
     callback();
   }, function(reason) {
     console.log('Error: ' + reason.result.error.message);
   });
 }
 
-function graphSleepData(options){
-  sleep_data = [];
-  for (i in all_activity_data) {
-    if (all_activity_data[i].dataset[0].point.length > 0) {
-      sleep_data.push([new Date(+all_activity_data[i].dataset[0].point[0].startTimeNanos/1000000), 0]);
-    }
-    for (j in all_activity_data[i].dataset[0].point){
-      if (SLEEP_NUMBERS.includes(+all_activity_data[i].dataset[0].point[j].value[0].intVal)) {
-        sleep_data[sleep_data.length-1][1] += +all_activity_data[i].dataset[0].point[j].value[1].intVal/3600000;
-      }
-    }
-  }
-  makeGraph(sleep_data, "#Sleep-graph", options);
-}
-
-function graphTotalActivityData(options){
-  data = [];
-  for (i in all_activity_data) {
-    if (all_activity_data[i].dataset[0].point.length > 0) {
-      data.push([new Date(+all_activity_data[i].dataset[0].point[0].startTimeNanos/1000000), 0]);
-    }
-    for (j in all_activity_data[i].dataset[0].point){
-      if (!NON_ACTIVE_NUMBERS.includes(+all_activity_data[i].dataset[0].point[j].value[0].intVal)) {
-        data[data.length-1][1] += +all_activity_data[i].dataset[0].point[j].value[1].intVal/3600000;
-      }
-    }
-  }
-  makeGraph(data, "#Active-time-graph", options);
-}
 
 
-function extractDataFromStreams(streams) {
-  var data = [];
-  for (stream in streams) {
-    var curr = streams[stream].result.point;
-    if (curr.length > data.length) {
-      data = curr;
-    }
-  }
-
-  return data;
-}
 
 function updatePreferences() {
   $.ajax("http://www.stability-app.com/update_settings", {
@@ -416,96 +385,4 @@ function restoreSettings(settings) {
       getActivity(callback);
     }
   }
-}
-
-function makeGraph(data, svg_id, options){
-  // Based on https://github.com/jasondavies/science.js/blob/master/examples/loess/loess.js
-
-
-  var id_without_hash = svg_id.slice(1, svg_id.length);
-
-  var w = document.getElementById(id_without_hash).offsetWidth,
-      h = document.getElementById(id_without_hash).offsetHeight,
-      p = 35.5,
-      n = 100;
-
-  console.log(h);
-
-  w -= 2*p; //margins need to come out of width
-  h -= 2*p; //margins need to come out of width
-
-  var min_band = 2/data.length;
-
-  var loess = science.stats.loess().bandwidth(d3.max([min_band, options.smoothing]));
-
-  var zipped_data = d3.transpose(data);
-  var original_data = d3.transpose(data);
-  var loess_result = loess(zipped_data[0], zipped_data[1]);
-
-  zipped_data[1] = loess_result.loess;
-  zipped_data.push(loess_result.confint);
-  data = d3.zip(zipped_data[0], zipped_data[1], zipped_data[2], original_data[1]);
-
-  max_y = d3.max(data, function(d){return d[2] ? d3.max([d[1]+d[2], d[3]]) : d3.max([d[1], d[3]]);});
-  min_y = d3.min(data, function(d){return d[2] ? d3.min([d[1]-d[2], d[3]]) : d3.min([d[1], d[3]]);});
-
-  //Add margins
-  max_y += max_y*.2
-  if (min_y > 0) {
-    min_y -= min_y*.2;
-  }
-
-  var x = d3.scaleTime().domain([new Date(data[0][0]), new Date(data[data.length-1][0])]).range([0, w]);
-  var y = d3.scaleLinear().domain([min_y, max_y]).range([h, 0]);
-
-  var xAxis = d3.axisBottom(x),
-      yAxis = d3.axisLeft(y);
-
-  var svg = d3.select(svg_id)
-    .append("svg")
-      .attr("width", w + p + p)
-      .attr("height", h + p + p);
-  var vis =  svg.append("g")
-      .attr("transform", "translate(" + p + "," + p + ")");
-
-  var area = d3.area()
-        .x(function(d) { return x(d[0]); })
-        .y0(function(d) { return d[2] ? y(d[1]-d[2]) : y(d[1]);})
-        .y1(function(d) { return d[2] ? y(d[1]+d[2]) : y(d[1]);});
-
-
-  if (options.confint){
-    vis.selectAll("path")
-        .data([data])
-      .enter().append("path")
-        .attr("d", function(d){return area(d);})
-        .attr("stroke", "black")
-        .attr("fill", "purple")
-        .attr("stroke-width", 1);
-  }
-
-  if (options.points){
-    vis.selectAll("circle")
-       .data(data)
-       .enter()
-       .append("circle")
-       .attr("r", 5)
-       .attr("cx", function(d){return x(d[0]);})
-       .attr("cy", function(d){return y(d[3]);});
-  }
-
-  vis.append("g")
-      .attr("class", "bottom axis")
-      .attr("transform", "translate(0," + h + ")")
-      .call(xAxis);
-
-  if (!options.y_axis_ticks) {
-    yAxis.ticks(0);
-  }
-
-  vis.append("g")
-      .attr("class", "left axis")
-      .call(yAxis);
-
-  graphs[id_without_hash] = {"x_axis":xAxis, "y_axis":yAxis, "vis":svg, "options":options};
 }
